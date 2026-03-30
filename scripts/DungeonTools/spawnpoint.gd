91ceb7e18cffa6dcf5e05b381bbf2d84036cfcb4
Isabel Lomas <me@isabel.gg>
2026-03-30 16:31:56 -0700

    feat: custom spawnpoint & exit zone types

---
+++ scripts/DungeonTools/spawnpoint.gd
@@ -0,0 +1,6 @@
+@icon("res://art/textures/icons/icon_map_pointer.svg")
+class_name Spawnpoint3D
+extends Node3D
+
+## A marker node that represents a spawnpoint in a scene.
+## Used by ExitZone3Ds to identify valid destinations.

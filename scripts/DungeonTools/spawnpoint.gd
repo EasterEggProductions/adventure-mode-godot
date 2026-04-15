de924602f510cfe7ea4f1211330d0f638534f3b2
Ryan <32203661+Visual-mov@users.noreply.github.com>
2026-04-15 15:51:01 -0700

    Spawnpoint editor gizmos (#34)
    * add subtypes for spawnpoints
    
    * add the gizmo models
    
    * Implement gizmos for spawnpoints using custom models
    
    ---------
    
    Co-authored-by: Isabel Lomas <me@isabel.gg>

--- scripts/DungeonTools/spawnpoint.gd
+++ scripts/DungeonTools/spawnpoint.gd
@@ -4,3 +4,11 @@ extends Node3D
 
 ## A marker node that represents a spawnpoint in a scene.
 ## Used by ExitZone3Ds to identify valid destinations.
+
+enum Subtype {
+	DEFAULT,
+	MULTIPLAYER,
+	WALK_IN,
+}
+
+@export var subtype : Subtype = Subtype.DEFAULT

830b1782e3791695b531e64e020bdbd125b4c284
KaletheQuick <thomashaynes@eastereggproductions.com>
2026-04-28 12:56:10 -0700

    Add swimmable area to Hitherton, Fix swim movement package, Fix Hitherton suspicious cant to the north

--- Modular Mobility/mvpk_areaDependant.gd
+++ Modular Mobility/mvpk_areaDependant.gd
@@ -17,8 +17,6 @@ func release_situation_check(thrall : Actor) -> bool:
 func shape_check(thrall : Actor) -> bool:
 	var shape = thrall.get_node("water_cast") #NOTE Stand in situation gantry
 	for x in range(shape.get_collision_count()):
-		#print(shape.get_collider(x).name)
-		
 		if is_instance_valid(shape.get_collider(x)) and  shape.get_collider(x).name == area_type:
 			return true
 	return false
@@ -28,12 +26,6 @@ func move_thrall(thrall : Actor, delta : float):
 	# Get the motion delta.
 	thrall.velocity = ((thrall.animation_tree.get_root_motion_rotation_accumulator().inverse() * thrall.get_quaternion()) * thrall.animation_tree.get_root_motion_position() / delta) * 2
 
-
-#	if movement_extra_expression != "":
-#		var expression = Expression.new()
-#		#expression.
-#		expression.parse(movement_extra_expression)
-#		expression.execute([],self)
 	# NOTE - Not specifically 'area dependant movement'
 	# NOTE - This is specific to swimming
 	var shape = thrall.get_node("water_cast") #NOTE Stand in situation gantry
@@ -41,28 +33,16 @@ func move_thrall(thrall : Actor, delta : float):
 	for x in range(shape.get_collision_count()):
 		#print(shape.get_collider(x).name)
 		if shape.get_collider(x).name == area_type:
-			var water_zone = shape.get_collider(x)
-			bouyancy = shape.global_position.y - (water_zone.global_position.y)
+			#var water_zone : Area3D = shape.get_collider(x)
 			# TODO - get top of water area
-			#print(bouyancy)
-			thrall.velocity.y = bouyancy + 0.05
+			var surface_height = -0.419204 # hard coded water height from blender geometry
+			bouyancy = shape.global_position.y - surface_height + 0.5
+			thrall.velocity.y = bouyancy# + 0.05
 			thrall.velocity.y = clampf(thrall.velocity.y, 0.5, -0.5)
 
 
-	# Add the gravity.
-	if not thrall.is_on_floor():# && desired_move.y < 0.1:
-		pass
-		#thrall.velocity.y = old_fallVel # + velocity.y
-		#thrall.velocity.y -= gravity * delta #no gravity for water lol
-		# NOTE Special case
-		#if gliding:
-		#	velocity.y = clampf(velocity.y, termnalVel, 999999999)
-	#print(animation_tree.get_root_motion_position().length())
-
-	# global_basis * (animation_tree.get_root_motion_position_accumulator())
 	thrall.quaternion = thrall.quaternion * ((thrall.animation_tree.get_root_motion_rotation() / delta) * 10)
-
-	thrall.move_and_slide()
+	thrall.combat_mode = false # FIXME - Better technique needed to prevent trying to enter combat states from swimming
 
 
 

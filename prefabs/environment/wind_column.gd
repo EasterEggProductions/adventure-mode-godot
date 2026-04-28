626495e4c72a0c076f1092b9934af2a2077456dc
Ryan D <ryandanver@gmail.com>
2026-04-28 14:32:12 -0700

    turn wind column into a dungeon object

--- prefabs/environment/wind_column.gd
+++ prefabs/environment/wind_column.gd
@@ -1,16 +1,19 @@
-extends Area3D
+extends DungeonObject
 class_name WindColumn3D
 
 ## Velocity (m/s) applied to actors gliding inside this column.
 @export var wind_velocity: Vector3 = Vector3(0, 3, 0)
+@export var enabled: bool = false
+
+@onready var _wind_area = $Area3D
 
 func _ready():
-	body_entered.connect(_on_body_entered)
-	body_exited.connect(_on_body_exited)
-	tree_exiting.connect(_on_tree_exiting)
+	_wind_area.body_entered.connect(_on_body_entered)
+	_wind_area.body_exited.connect(_on_body_exited)
+	_wind_area.tree_exiting.connect(_on_tree_exiting)
 
 func _on_body_entered(body: Node3D):
-	if body is Actor:
+	if body is Actor and enabled:
 		body.wind_columns_inside.append(self)
 
 func _on_body_exited(body: Node3D):
@@ -21,3 +24,9 @@ func _on_tree_exiting():
 	for actor in Actor.ALL_EVER_MADE:
 		if is_instance_valid(actor):
 			actor.wind_columns_inside.erase(self)
+
+func serialize() -> Dictionary:
+	return {"enabled": enabled}
+	
+func deserialize(state: Dictionary) -> void:
+	self.enabled = state["enabled"]
